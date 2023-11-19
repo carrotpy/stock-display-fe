88af78a605f712dd3abd7552c0876de4021059ec
jai4sankar <jai4sankar@users.noreply.github.com>
2023-11-19 12:51:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route,Routes} from "react-router"
+import MainTable from "./MainPage/TableHeader"
+import Addbutton from './MainPage/Addproduct';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <Routes>
+        <Route path='/' element={<MainTable />}/>
+        <Route path='/1' element={<Addbutton/>}/>
+        </Routes>
+      
     </div>
   );
 }
