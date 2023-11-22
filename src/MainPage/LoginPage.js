7b6c48352478b72a28623fe3020fb7ebfc14da6e
Dhilip R <48360598+carrotpy@users.noreply.github.com>
2023-11-22 23:37:01 +0530

    working login

--- src/MainPage/LoginPage.js
+++ src/MainPage/LoginPage.js
@@ -1,18 +1,55 @@
-/*
-  This Loginpage requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
-export default function Loginpage() {
+import React, { useState } from "react";
+import validator from "validator";
+import { Navigate } from "react-router-dom";
+
+// var passw
+var needToCheck = false;
+var needToCheckpas = false;
+
+const Login = () => {
+  var [email, setEmail] = useState(" ");
+  var [passw, setPassw] = useState(" ");
+  const submitThis = (e) => {
+    e.preventDefault();
+
+    if (needToCheck && needToCheckpas) {
+      //need to add firebase authicator
+      const info = { email: email, passw: passw };
+      let validateResult = true;
+      document.cookie = `validate_email=${email}`;
+
+      if (validateResult === true) {
+        localStorage.setItem("authenticated", true);
+      }
+    } else {
+      localStorage.setItem("authenticated", false);
+      console.log("thapacreds");
+    }
+  };
+  const emailvalidate = (value) => {
+    // setEmail(value);
+    if (validator.isEmail(value)) {
+      setEmail(value);
+      // console.log(`email is ${value}`);
+      needToCheck = true;
+      console.log(`email is ${value}   ${needToCheck}`);
+    } else {
+      needToCheck = false;
+      console.log(`bad email`);
+    }
+  };
+  const passvalidate = (value) => {
+    // setPassw(value);
+    if (validator.isStrongPassword(value)) {
+      setPassw(value);
+      needToCheckpas = true;
+      console.log(`password is ${value}  ${needToCheckpas}`);
+    } else {
+      needToCheckpas = false;
+      console.log(`bad pass ${passw}`);
+    }
+  };
+
   return (
     <>
       {/*
@@ -27,7 +64,7 @@ export default function Loginpage() {
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
             className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            src="https://img.icons8.com/emoji/47/rosette.png"
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
@@ -36,7 +73,12 @@ export default function Loginpage() {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+          <form
+            className="space-y-6"
+            action="#"
+            method="POST"
+            onSubmit={(e) => submitThis(e)}
+          >
             <div>
               <label
                 htmlFor="email"
@@ -50,7 +92,8 @@ export default function Loginpage() {
                   name="email"
                   type="email"
                   autoComplete="email"
-                  required
+                  onChange={(e) => emailvalidate(e.target.value)}
+                  required={true}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -79,7 +122,8 @@ export default function Loginpage() {
                   name="password"
                   type="password"
                   autoComplete="current-password"
-                  required
+                  onChange={(e) => passvalidate(e.target.value)}
+                  required={true}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -108,4 +152,5 @@ export default function Loginpage() {
       </div>
     </>
   );
-}
+};
+export default Login;
