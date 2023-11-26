6e6dcc376c0bb1adcf87bd27f135b30fc39a5dfd
jai4sankar <jai4sankar@users.noreply.github.com>
2023-11-26 18:13:49 +0530

    adding scv

--- src/MainPage/EditProduct.js
+++ src/MainPage/EditProduct.js
@@ -33,6 +33,7 @@ const Editbutton = (info) => {
 
     navigate("/");
   };
+
   return (
     // <!-- Main modal -->
     <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
--- src/MainPage/Footer.js
+++ src/MainPage/Footer.js
@@ -12,6 +12,36 @@ function FooterBar() {
         </span>
         <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+              Made with{"   "}
+            </span>
+          </li>
+          <li>
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+              <svg
+                width="20px"
+                height="20px"
+                viewBox="0 0 64 64"
+                aria-hidden="true"
+                role="img"
+                class="iconify iconify--emojione"
+                preserveAspectRatio="xMidYMid meet"
+              >
+                <path
+                  d="M61.1 18.2c-6.4-17-27.2-9.4-29.1-.9c-2.6-9-22.9-15.7-29.1.9C-4 36.7 29.6 53.3 32 56c2.4-2.2 36-19.6 29.1-37.8"
+                  fill="#f5ce3e"
+                ></path>
+              </svg>
+            </span>
+          </li>
+
+          <li>
+            <a> in India</a>
+          </li>
+        </ul>
+
+        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li>
             <a href="#" class="hover:underline me-4 md:me-6">
               About
             </a>
